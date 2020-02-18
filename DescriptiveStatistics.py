61d343710e9c63f2664b5a6d659870bb080f1019
nhossain819 <43657615+nhossain819@users.noreply.github.com>
2020-02-18 12:07:02 -0500

    Create DescriptiveStatistics.py

---
+++ DescriptiveStatistics.py
@@ -0,0 +1,13 @@
+"""
+The following lines:
+-Provide descriptive statistics by a specific column.
+"""
+average_carat = df.carat.mean()
+
+max_carat = df.carat.max()
+
+min_carat = df.carat.min()
+
+median_carat = df.carat.median()
+
+sum_carat = df.carat.sum()
